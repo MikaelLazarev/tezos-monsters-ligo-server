06adaa634d4d6425683362ec1efeb174f3316e60
Mikhail Lazarev <mikael.lazarev@gmail.com>
2020-06-23 17:52:59 +0300

    remove static files handlers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import cors from 'cors';
 import express from 'express';
-import fs from 'fs';
-import { dirname, join } from 'path';
 
 import { compileContractHandler } from './handlers/compile-contract';
 import { compileExpressionHandler } from './handlers/compile-expression';
@@ -11,8 +9,6 @@ import { dryRunHandler } from './handlers/dry-run';
 import { evaluateValueHandler } from './handlers/evaluate-value';
 import { runFunctionHandler } from './handlers/run-function';
 import { shareHandler } from './handlers/share';
-import { sharedLinkHandler } from './handlers/shared-link';
-import { loadDefaultState } from './load-state';
 import { errorLoggerMiddleware, loggerMiddleware } from './logger';
 require('./metrics');
 
@@ -26,18 +22,9 @@ const APP_PORT = 8080;
 const metrics = express();
 const METRICS_PORT = 8081;
 
-const corsOptions = {
-  origin: 
-    '*'
-  
-};
+const corsOptions = {origin: '*'};
 
 app.use(cors(corsOptions));
-const appRootDirectory =
-  process.env['STATIC_ASSETS'] ||
-  dirname(require.resolve('../../client/package.json'));
-const appBundleDirectory = join(appRootDirectory, 'build');
-
 app.use(bodyParser.json());
 app.use(loggerMiddleware);
 app.use(
@@ -50,31 +37,9 @@ app.use(
   })
 );
 
-const file = fs.readFileSync(join(appBundleDirectory, 'index.html'));
-
-const template = (defaultState: string = '{}') => {
-  return file.toString().replace(
-    `<div id="root"></div>`,
-    // Injecting a script that contains a default state (Might want to refactor this if we do ssr)
-    // Adding an div containing the initial state this is vulnerable to xss
-    // To avoid vulnerability we escape it and then parse the content into a global variable
-    `
-     <input type="hidden" id="initialState" value="${escape(defaultState)}" />
-     <div id="root"></div>
-     <script>var defaultServerState = JSON.parse(document.getElementById("initialState").value); document.getElementById("initialState").remove()</script>`
-  );
-};
-app.use('^/$', async (_, res) =>
-  res.send(template(JSON.stringify(await loadDefaultState(appBundleDirectory))))
-);
-app.use(express.static(appBundleDirectory));
 
 app.options('/api/share', cors(corsOptions));
 
-app.get(
-  `/p/:hash([0-9a-zA-Z\-\_]+)`,
-  sharedLinkHandler(appBundleDirectory, template)
-);
 app.post('/api/compile-contract', compileContractHandler);
 app.post('/api/compile-expression', compileExpressionHandler);
 app.post('/api/compile-storage', compileStorageHandler);
